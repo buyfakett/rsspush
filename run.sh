c9a8026c0edc3689a3682ca4b4ddf91662f76a6e
buyfakett <buyfakett@vip.qq.com>
2023-10-31 14:31:48 +0800

    :sparkles: feat: 一键启动测试-3

--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@ if [[ ! -f database]]; then
   python3 manage.py makemigrations push
   python3 manage.py makemigrations user
   python3 manage.py migrate
+  touch database
 fi
 python3 manage.py runserver 0.0.0.0:8000
